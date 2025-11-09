7a912a80d2111991296dd08149d76811ec970674
anayk13 <anaysk13@gmail.com>
2025-11-10 00:27:37 +0530

    Initial Commit

---
+++ test_google.py
@@ -0,0 +1,7 @@
+from selenium import webdriver
+
+def test_google_search():
+    driver = webdriver.Chrome()
+    driver.get("https://google.com")
+    assert "Google" in driver.title
+    driver.quit()
